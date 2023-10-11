126a8764019c90806a2f059a291d4c0705b34a1f
Polosiv Kit <borsseckij08258@gmail.com>
2023-10-11 14:35:42 +1000

    final comit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+counter = 0
+
+@app.route("/image")
+def image():
+    global counter
+    return render_template('image.html', counter=counter)
+
+@app.route('/button', methods=['GET', 'POST'])
+def button():
+    global counter
+    if request.method == 'POST':
+        counter += 1
+        counter %= 3
+    return render_template('button.html')
+
+
+
+
+
+
